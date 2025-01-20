53ce55261a4a0dcb6fe92c06c9e69d97b6fae802
EPS1L0N4IK <strelcov.ilia.2011@gmail.com>
2025-01-20 20:48:48 +0600

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -24,16 +24,16 @@ class Player(GameSprite):
    # метод для управления спрайтом стрелками клавиатуры
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 80:
+        if keys[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
     
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
 racket_image = 'racket.png'
@@ -71,3 +71,4 @@ while game:
     
     display.update()
     clock.tick(FPS)
+
